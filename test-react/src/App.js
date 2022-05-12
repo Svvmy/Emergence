f5d914ac7436bcdcbcfe9b751c7c33a13f3f1187
Yunus <sunner9155@gmail.com>
2022-05-12 16:22:57 +0200

    Ajout de route

--- test-react/src/App.js
+++ test-react/src/App.js
@@ -1,12 +1,15 @@
 import './App.css'
 import {Routes, Route} from "react-router-dom"
-import Home from './components/Home/Home'
+import Accueil from './components/Home/Accueil'
 import Profile from './components/Profile/Profile'
 import Navbar from './components/Navbar/Navbar'
 import Post from './components/Profile/Posts/Posts'
 import Experience from './components/Profile/Experiences/Experiences'
 import Projet from './components/Profile/Projets/Projets'
 import Messagerie from './components/Profile/Messagerie/Messagerie'
+import SignInMail from './components/Home/SignInMail/SignInMail'
+import Login from './components/Home/Login/Login'
+import Home from './components/Home/Home'
 
 
 export default function App() {
@@ -14,12 +17,16 @@ export default function App() {
     <div className='App'>
       <Navbar />
       <Routes>
-        <Route path ="/" element ={<Home />} />
+        <Route path ="/" element ={<Accueil />} />
+        <Route path ="/home" element ={<Home />} />
+        <Route path ="/signin_mail" element ={<SignInMail />} />
+        <Route path ="/login" element ={<Login />} />
         <Route path ="/profil/:id" element ={<Profile />}>
           <Route path ="/profil/:id/post" element ={<Post />} />
           <Route path ="/profil/:id/experience" element ={<Experience />} />
           <Route path ="/profil/:id/projet" element ={<Projet />} />
           <Route path ="/profil/:id/messagerie" element ={<Messagerie />} />
+
         </Route>     
       </Routes>
     </div>
